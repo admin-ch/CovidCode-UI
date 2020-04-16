05455f94dcdfdea886f025b221e55de5476867f2
René Giller <rene.giller@bit.admin.ch>
2020-04-16 18:04:21 +0200

    feat(generate-code): keep date locale in sync with lang

--- src/app/generate-code/generate-code.module.ts
+++ src/app/generate-code/generate-code.module.ts
@@ -6,6 +6,9 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatDialogModule} from '@angular/material/dialog';
+import {DateAdapter} from '@angular/material/core';
+import {TranslateService} from '@ngx-translate/core';
+import {map} from 'rxjs/operators';
 import {ObErrorMessagesModule, ObInputClearModule} from '@oblique/oblique';
 import {SharedModule} from 'shared/shared.module';
 import {GenerateCodeComponent} from './generate-code.component';
@@ -26,4 +29,13 @@ import {CodeComponent} from './code/code.component';
 		ObInputClearModule
 	]
 })
-export class GenerateCodeModule {}
+export class GenerateCodeModule {
+	constructor(adapter: DateAdapter<any>, translate: TranslateService) {
+		translate.onLangChange
+			.pipe(
+				map(lang => lang.lang),
+				map(lang => `${lang}-CH`)
+			)
+			.subscribe(locale => adapter.setLocale(locale));
+	}
+}
