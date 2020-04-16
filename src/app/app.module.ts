5c6b0a31b17d9b992cf8603ae4defd123beb6e81
René Giller <rene.giller@bit.admin.ch>
2020-04-16 17:54:58 +0200

    feat(toolchain): register more locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TranslateModule} from '@ngx-translate/core';
 import {multiTranslateLoader, ObHttpApiInterceptor, ObMasterLayoutModule} from '@oblique/oblique';
-
+import {registerLocaleData} from '@angular/common';
+import localeDECH from '@angular/common/locales/de-CH';
+import localeFRCH from '@angular/common/locales/fr-CH';
+import localeITCH from '@angular/common/locales/it-CH';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+
+registerLocaleData(localeDECH);
+registerLocaleData(localeFRCH);
+registerLocaleData(localeITCH);
 
 @NgModule({
 	declarations: [AppComponent],
