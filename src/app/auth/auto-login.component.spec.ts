cc36d210a2ee0828e9cfff391c1ae50cb7326172
René Giller <rene.giller@bit.admin.ch>
2020-05-01 18:36:25 +0200

    refactor(security): simplify `auto-login`

--- src/app/auth/auto-login.component.spec.ts
+++ src/app/auth/auto-login.component.spec.ts
@@ -1,28 +1,28 @@
-import {AutoLoginComponent} from './auto-login.component';
 import {TestBed} from '@angular/core/testing';
+import {of} from 'rxjs';
 import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {AutoLoginComponent} from './auto-login.component';
+import {OauthService} from './oauth.service';
 
 describe('AutoLoginComponent', () => {
-	const securityService = {
-		onModuleSetup: jest.fn(),
-		moduleSetup: jest.fn(),
-		authorize: jest.fn()
-	};
-
+	const mock = {authorize: jest.fn()};
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			declarations: [AutoLoginComponent],
-			providers: [{provide: OidcSecurityService, useValue: securityService}]
+			providers: [
+				{provide: OidcSecurityService, useValue: mock},
+				{provide: OauthService, useValue: {setup$: of(true)}}
+			]
 		}).compileComponents();
 	});
 
 	it('should create', () => {
 		const fixture = TestBed.createComponent(AutoLoginComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app).toBeTruthy();
+		const component = fixture.debugElement.componentInstance;
+		expect(component).toBeTruthy();
 	});
 
-	afterAll(() => {
-		TestBed.resetTestingModule();
+	it('should call authorize once', () => {
+		expect(mock.authorize).toHaveBeenCalled();
 	});
 });
--- src/app/auth/auto-login.component.ts
+++ src/app/auth/auto-login.component.ts
@@ -1,28 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {OauthService} from './oauth.service';
 
 @Component({
 	selector: 'ha-auto-component',
 	templateUrl: './empty.component.html'
 })
-export class AutoLoginComponent implements OnInit {
-	constructor(private readonly securityService: OidcSecurityService) {
-		if (this.securityService.moduleSetup) {
-			this.onModuleSetup();
-		} else {
-			this.securityService.onModuleSetup.subscribe(() => {
-				this.onModuleSetup();
-			});
-		}
-	}
-
-	ngOnInit() {
-		if (this.securityService.moduleSetup) {
-			this.onModuleSetup();
-		}
-	}
-
-	private onModuleSetup() {
-		this.securityService.authorize();
+export class AutoLoginComponent {
+	constructor(securityService: OidcSecurityService, oauth: OauthService) {
+		oauth.setup$.subscribe(() => securityService.authorize());
 	}
 }
