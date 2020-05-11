40a8473e7fc1082ecde017e60b9f2ef2f7f740be
Rene Giller <rene.giller@bit.admin.ch>
2020-05-11 08:25:05 +0200

    feat(home): preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {AuthGuardService} from './auth/auth-guard.service';
 
 const routes: Routes = [
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
