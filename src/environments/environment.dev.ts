a0e29d0e9521e984276ccfbe81b01b7ecaf5c1ac
Rene Giller <rene.giller@bit.admin.ch>
2020-05-13 20:43:22 +0200

    fix(toolchain): set production env to `true` for dev, abn & test
    PTAPP-486

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
+	production: true,
 	showWarning: true,
 	host: 'https://codegen-service-d.bag.admin.ch',
 	eiamSelfAdmin:
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
+	production: true,
 	showWarning: true,
 	host: 'https://codegen-service-t.bag.admin.ch',
 	eiamSelfAdmin:
