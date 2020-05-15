7cf84b7ebb094c2c9af12c469c49c4a735591596
Rene Giller <rene.giller@bit.admin.ch>
2020-05-15 13:04:57 +0200

    feat(toolchain): add `abn` env

---
+++ src/environments/environment.abn.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+	production: false,
+	host: 'https://codegen-service-a.bag.admin.ch',
+	eiamSelfAdmin: 'https://sts-a.pts.admin.ch/_pep/myaccount?returnURL=https%3A%2F%2Fwww.covidcode-a.admin.ch',
+	oidc: {
+		clientId: 'ha-ui',
+		afterLoginPath: '/generate-code',
+		stsServer: 'https://identity-a.bit.admin.ch/realms/bag-pts',
+		applicationUrl: 'https://www.covidcode-a.admin.ch/',
+		post_logout_redirect_uri: 'https://www.covidcode-a.admin.ch/',
+		silentRenew: false,
+		useAutoLogin: false,
+		debug: true,
+		token_aware_url_patterns: ['/v1/(authcode).*']
+	}
+};
