50b239b790a3264bfc72789e0bb27968e00c413e
René Giller <rene.giller@bit.admin.ch>
2020-08-18 16:31:33 +0200

    refactor(toolchain): fix Sonar findings
    PTAPP-662

--- src/app/auth/open-id-config-service.ts
+++ src/app/auth/open-id-config-service.ts
@@ -22,10 +22,10 @@ export class OpenIdConfigService {
 	readonly config: OpenIdConfiguration = {
 		client_id: environment.oidc.clientId,
 		stsServer: environment.oidc.stsServer,
-		redirect_url: environment.oidc.applicationUrl + environment.oidc.loginFeedback,
-		silent_renew_url: environment.oidc.applicationUrl + 'assets/auth/silent-refresh.html',
+		redirect_url: `${environment.oidc.applicationUrl}environment.oidc.loginFeedback`,
+		silent_renew_url: `${environment.oidc.applicationUrl}assets/auth/silent-refresh.html`,
 		post_logout_redirect_uri: environment.oidc.applicationUrl,
-		post_login_route: '/' + environment.oidc.afterLoginPath,
+		post_login_route: `/${environment.oidc.afterLoginPath}`,
 		log_console_debug_active: environment.oidc.debug,
 		response_type: 'code',
 		start_checksession: false,
