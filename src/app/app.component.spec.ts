0ee0925c9e889ea3ae9c1ad5e7d172b7219dee31
René Giller <rene.giller@bit.admin.ch>
2020-09-02 14:21:19 +0200

    fix(security): refactor to oidc client update
    #11

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import {async, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {Router} from '@angular/router';
 import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
 import {ObliqueTestingModule} from '@oblique/oblique';
 import {OidcSecurityService} from 'angular-auth-oidc-client';
@@ -24,7 +23,7 @@ describe('AppComponent', () => {
 			declarations: [AppComponent],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 			providers: [
-				{provide: OidcSecurityService, useValue: {getIsAuthorized: () => of(false)}},
+				{provide: OidcSecurityService, useValue: {isAuthenticated$: of(false)}},
 				{provide: OauthService, useValue: mock}
 			]
 		}).compileComponents();
